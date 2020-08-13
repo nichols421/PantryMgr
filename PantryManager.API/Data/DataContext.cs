543eae35f3899ce9889402ccfc618e0981c44962
Nychols <enichols@glassfabtempering.com>
2020-08-13 12:29:53 -0700

    Initial Commit

---
+++ PantryManager.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using PantryManager.API.Models;
+
+namespace PantryManager.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+        public DbSet<Values> values { get; set; }
+    }
+}
---
+++ PantryManager.API/Models/Values.cs
@@ -0,0 +1,8 @@
+namespace PantryManager.API.Models
+{
+    public class Values
+    {
+        public int id { get; set; }
+        public string  name { get; set; }
+    }
+}
